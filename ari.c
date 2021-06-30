08015b22513ca4a5b921d2fe9184e6ce184f27a8
Amit780 <85019843+Amit780@users.noreply.github.com>
2021-06-30 21:34:01 +0530

    sl

--- ari.c
+++ ari.c
@@ -3,7 +3,7 @@ string ari ( string s ){
 float chr=0;
 float wrd=0;
 float sen =0;
-int ari;
+int ar;
 float fr1, fr2;
 for( int i=0; s[i]!= '\0'; i++){
 if(isalnum(s[i]))
@@ -15,33 +15,33 @@ sen = sen + 1;
 }
   fr1 = chr/wrd;
   fr2= wrd/sen;
-ari = ceil(4.71*(fr1) + 0.5*(fr2) - 21.43);
-if(ari == 1)
+ar = ceil(4.71*(fr1) + 0.5*(fr2) - 21.43);
+if(ar == 1)
 return "Kindergarten";
-else if(ari == 2)
+else if(ar == 2)
 return "First/Second Grade";
-else if(ari == 3) 
+else if(ar == 3) 
 return "Third Grade";
-else if(ari == 4)
+else if(ar == 4)
 return "Fourth Grade";
-else if(ari == 5)
+else if(ar == 5)
 return "Fifth Grade";
-else if(ari == 6)
+else if(ar == 6)
 return "Sixth Grade";
-else if(ari == 7)
+else if(ar == 7)
 return "Seventh Grade";
-else if(ari == 8)
+else if(ar == 8)
 return "Eighth Grade Grade";
-else if(ari == 9)
+else if(ar == 9)
 return "Ninth Grade";
-else if(ari == 10)
+else if(ar == 10)
 return "Tenth Grade";
-else if(ari == 11)
+else if(ar == 11)
 return "Eleventh Grade";
-else if(ari == 12)
+else if(ar == 12)
 return "Twelfth grade";
-else if(ari == 13)
+else if(ar == 13)
 return "College student";
-else if(ari == 14)
+else if(ar == 14)
 return "Professor";
 }
